0f611b1317fd590708e485ab8b2d6d66d983f2f6
Thiago <thiagowirzdemoura@gmail.com>
2022-05-08 01:19:16 -0300

    Initial Commit

---
+++ tests/integration/service.test.ts
@@ -0,0 +1,8 @@
+import supertest from "supertest";
+import app from "../../src/app.js";
+import { prisma } from "../../src/database.js";
+import { faker } from "@faker-js/faker";
+
+describe("POST recomedations", () => {
+  it("should return 201 given a valid body", async () => {});
+});
